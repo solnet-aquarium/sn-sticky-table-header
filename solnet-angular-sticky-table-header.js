6fe0c6e518ab89b4d438e8fd943e7519b9363ad7
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-11-26 11:59:09 +1300

    Updated to work in Chrome & IE9

--- solnet-angular-sticky-table-header.js
+++ solnet-angular-sticky-table-header.js
@@ -1,87 +1,86 @@
 /*jshint loopfunc: true */
 (function() {
 
-    /**
-     * Cross browser scrollTop without jQuery: http://stackoverflow.com/a/872537/187954
-     *
-     * @return {Number} The scrollTop position in pixels
-     */
-    function getScrollTop() {
-        if (typeof pageYOffset != 'undefined') {
-            return pageYOffset; // most browsers except IE before #9
-        } else {
-            var B = document.body; // IE 'quirks'
-            var D = document.documentElement; // IE with doctype
-            return (D.clientHeight ? D : B).scrollTop;
-        }
-    }
-
-    var tableHeaders = [],
-        STICKY = 'solnet-angular-sticky-table-header__original',
-        STICKY_FORMATTED = 'solnet-angular-sticky-table-header__original-formatted',
-        STICKING = 'solnet-angular-sticky-table-header__original--sticking',
-        CLONE = 'solnet-angular-sticky-table-header__clone',
-        CLONE_VISIBLE = 'solnet-angular-sticky-table-header__clone--visible';
-
-    var scrollHandler = function($window) {
-        return function(event) {
-
-            if (!tableHeaders.length) {
-                return;
-            }
-
-            var scrollTop = $($window).scrollTop();
-
-            for (var tableHeadersIndex = 0; tableHeadersIndex < tableHeaders.length; ++tableHeadersIndex) {
-                var theadPair = tableHeaders[tableHeadersIndex];
-                var thead = theadPair.thead;
-                var table = thead.parent();
-
-                if (thead.hasClass(STICKING)) {
-                    return;
-                }
-
-                if (!thead.hasClass(STICKY) && scrollTop > thead.offset().top) {
-                    if (!theadPair.theadClone) {
-                        theadPair.theadClone = thead.clone(true, true);
-                        thead.after(theadPair.theadClone);
-                    }
-                    thead.addClass(STICKING);
-                    var theadClone = theadPair.theadClone;
-                    theadClone.addClass(CLONE + ' ' + CLONE_VISIBLE);
-                    theadClone.css({
-                        left: thead.offset().left
-                    });
-                    if (!thead.hasClass(STICKY_FORMATTED)) {
-                        th = thead.find('th');
-                        theadClone.find('th').each(function(thCloneIndex, thClone) {
-                            return $(thClone).css({
-                                width: $(th.get(thCloneIndex)).width()
-                            });
-                        });
-                        thead.addClass(STICKY_FORMATTED);
-                    }
-                    thead.addClass(STICKY);
-                    thead.removeClass(STICKING);
-                }
-                if (thead.hasClass(STICKY) && scrollTop < table.offset().top) {
-                    thead.removeClass(STICKY);
-                    return theadPair.theadClone.removeClass(CLONE_VISIBLE);
-                }
-            }
-        };
-    };
-
     angular.module('solnetAngularStickyTableHeader', [])
-        .directive('solnetAngularStickyTableHeader', ['$window',
-            function($window) {
+        .value('solnetAngularStickyTableHeaderOptions', {
+            STICKY: 'solnet-angular-sticky-table-header__original',
+            CLONE: 'solnet-angular-sticky-table__clone',
+            CLONE_VISIBLE: 'solnet-angular-sticky-table__clone--visible'
+        })
+        .directive('solnetAngularStickyTableHeader', ['$window', 'solnetAngularStickyTableHeaderOptions',
+            function($window, solnetAngularStickyTableHeaderOptions) {
                 return {
                     restrict: 'A',
                     link: function(scope, element, attributes) {
-                        tableHeaders.push({
-                                table: $(element)
+                        var options = solnetAngularStickyTableHeaderOptions;
+
+                        angular.extend(scope, {
+                            thead: $(element).find('> :first-child'),
+                            table: $(element),
+                            clone: null,
+
+                            initialise: function() {
+                                $($window).bind('scroll', scope.scroll);
+                            },
+
+                            scroll: function(event) {
+                                var scrollTop = $($window).scrollTop();
+
+                                if (!scope.thead.hasClass(options.STICKY) && scrollTop > scope.table.offset().top) {
+                                    scope.stick();
+                                }
+
+                                if (scope.thead.hasClass(options.STICKY) && scrollTop < scope.table.offset().top) {
+                                    scope.unstick();
+                                }
+
+                                return true;
+                            },
+
+                            stick: function() {
+
+                                if (scope.clone === null) {
+                                    var table = scope.table,
+                                        clone = scope.table.clone(true, true);
+
+                                    clone.find('tbody').remove();
+                                    clone.css({
+                                        width: table.width(),
+                                        left: table.offset().left,
+                                        height: 'auto'
+                                    });
+
+                                    scope.clone = clone;
+                                    table.after(clone);
+                                }
+
+                                scope.styleClone();
+                                scope.clone.addClass(options.CLONE + ' ' + options.CLONE_VISIBLE);
+                                scope.thead.addClass(options.STICKY);
+                            },
+
+                            unstick: function() {
+                                scope.thead.removeClass(options.STICKY);
+                                scope.clone.removeClass(options.CLONE_VISIBLE);
+                            },
+
+                            styleClone: function() {
+                                th = scope.thead.find('th');
+                                scope.clone.find('th').each(function(thCloneIndex, thClone) {
+                                    $(thClone).css({
+                                        width: $(th.get(thCloneIndex)).width()
+                                    });
+                                });
+                            },
+
+                            destroy: function() {
+                                $($window).off(scope.scroll);
+                            }
                         });
-                        angular.element($window).bind('scroll', scrollHandler($window));
+
+                        scope.$on('$destroy', scope.destroy);
+
+                        scope.initialise();
                     }
                 };
             }
