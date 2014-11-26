2b0c2833feb1c471f39f684f5c4ba3a81d0c6591
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-11-27 09:08:26 +1300

    Updated coverage reporters

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,11 @@
 module.exports = function(config) {
     config.set({
-        files: [
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/angular/angular.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            'src/solnet-angular-sticky-table-header.js',
-            'test/unit/**/*.js'
+            files: [
+                'bower_components/jquery/dist/jquery.js',
+                'bower_components/angular/angular.js',
+                'bower_components/angular-mocks/angular-mocks.js',
+                'src/solnet-angular-sticky-table-header.js',
+                'test/unit/**/*.js'
         ],
         autoWatch: true,
         browsers: ['PhantomJS'],
@@ -19,6 +19,12 @@ module.exports = function(config) {
             'karma-coverage',
             'karma-jasmine',
             'karma-junit-reporter'
+        ],
+        coverageReporter: {
+            reporters: [
+                { type: 'html', subdir: 'report-html' },
+                { type: 'lcov', subdir: 'report-lcov' }
         ]
+        }
     });
-};
+    };
