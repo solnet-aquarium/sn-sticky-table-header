da7109b7d24eb94dacab6bc89eefe1e2a5289db2
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-12-23 10:07:58 +1300

    Reverted previous 2 incorrect commits

--- src/sn-sticky-table-header.js
+++ src/sn-sticky-table-header.js
@@ -16,6 +16,7 @@
                         var options = snStickyTableHeaderOptions;
 
                         angular.extend(scope, {
+                            thead: element.find('> :first-child'),
                             table: element,
                             clone: null,
 
@@ -26,11 +27,11 @@
                             scroll: function(event) {
                                 var scrollTop = $($window).scrollTop();
 
-                                if (!scope.table.hasClass(options.STICKY) && scrollTop > scope.table.offset().top) {
+                                if (!scope.thead.hasClass(options.STICKY) && scrollTop > scope.table.offset().top) {
                                     scope.stick();
                                 }
 
-                                if (scope.table.hasClass(options.STICKY) && scrollTop <= scope.table.offset().top) {
+                                if (scope.thead.hasClass(options.STICKY) && scrollTop <= scope.table.offset().top) {
                                     scope.unstick();
                                 }
 
@@ -45,11 +46,11 @@
 
                                 scope.styleClone();
                                 scope.clone.addClass(options.CLONE + ' ' + options.CLONE_VISIBLE);
-                                scope.table.addClass(options.STICKY);
+                                scope.thead.addClass(options.STICKY);
                             },
 
                             unstick: function() {
-                                scope.table.removeClass(options.STICKY);
+                                scope.thead.removeClass(options.STICKY);
                                 scope.clone.removeClass(options.CLONE_VISIBLE);
                             },
 
@@ -69,7 +70,7 @@
                             },
 
                             styleClone: function() {
-                                var th = scope.table.find('th');
+                                var th = scope.thead.find('th');
                                 angular.forEach(scope.clone.find('th'), function(thClone, thCloneIndex) {
                                     angular.element(thClone).css({
                                         width: $(th.get(thCloneIndex)).width()
