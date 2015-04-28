81721b70de450cba2af17a1f9cc18a3dcbd6b858
Michael Robinson <michael.robinson@kiwirail.co.nz>
2015-04-28 13:26:38 +1200

    Added basepath to karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,29 +1,34 @@
 module.exports = function(config) {
-    config.set({
-            files: [
-                'bower_components/jquery/dist/jquery.js',
-                'bower_components/angular/angular.js',
-                'bower_components/angular-mocks/angular-mocks.js',
-                'src/sn-sticky-table-header.js',
-                'test/unit/**/*.js'
-        ],
-        browsers: ['PhantomJS'],
-        frameworks: ['jasmine'],
-        reporters: ['progress', 'coverage'],
-        preprocessors: {
-            'src/**/*.js': ['coverage']
-        },
-        plugins: [
-            'karma-phantomjs-launcher',
-            'karma-coverage',
-            'karma-jasmine',
-            'karma-junit-reporter'
-        ],
-        coverageReporter: {
-            reporters: [
-                { type: 'html', subdir: 'report-html' },
-                { type: 'lcov', subdir: 'report-lcov' }
-        ]
-        }
-    });
-    };
+  config.set({
+      basePath: '',
+    files: [
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'src/sn-sticky-table-header.js',
+      'test/unit/**/*.js'
+    ],
+    browsers: ['PhantomJS'],
+    frameworks: ['jasmine'],
+    reporters: ['progress', 'coverage'],
+    preprocessors: {
+      'src/**/*.js': ['coverage']
+    },
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-coverage',
+      'karma-jasmine',
+      'karma-junit-reporter'
+    ],
+    coverageReporter: {
+      reporters: [{
+        type: 'html',
+        subdir: 'report-html'
+      }, {
+        type: 'lcov',
+        subdir: 'report-lcov'
+      }]
+    }
+  });
+};
+
