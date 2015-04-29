9bf66dc22446268d66ce35a404d74f5e16ec9827
Michael Robinson <michael.robinson@kiwirail.co.nz>
2015-04-30 11:03:10 +1200

    Added TDD tests for scroll to top button

--- test/protractor/test.js
+++ test/protractor/test.js
@@ -9,5 +9,31 @@ describe('sn-sticky-table-header', function() {
       expect(element.all(by.css('.sn-sticky-table__clone--visible')).count()).toBe(1);
     });
   });
+
+  it('should display the "scroll to top" button when the secondary thead is visible and hovered', function() {
+    var secondaryThead = element.all(by.css('.sn-sticky-table__clone--visible'));
+    var scrollToTopButton = element.all(by.css('.sn-sticky-table__clone__scroll-button'));
+    browser.actions()
+      .mouseMove(secondaryThead.get(0))
+      .perform();
+
+    expect(scrollToTopButton.count()).toEqual(0);
+  });
+
+  it('should scroll to top when "scroll to top" button is clicked', function() {
+    var secondaryThead = element.all(by.css('.sn-sticky-table__clone--visible'));
+    var scrollToTopButton = element.all(by.css('.sn-sticky-table__clone__scroll-button'));
+
+    browser.actions()
+      .mouseMove(secondaryThead.get(0))
+      .perform();
+    browser.actions()
+      .mouseClick(secondaryThead.get(0))
+      .perform();
+
+    browser.executeScript('return window.pageYOffset').then(function(yPosition) {
+      expect(yPosition).toEqual(0);
+    });
+  });
 });
 
