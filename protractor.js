f79fcd684463981f65f3661231b52dae2a470ac5
Michael Robinson <michael.robinson@solnet.co.nz>
2015-08-26 10:45:18 +1200

    Protractor

--- protractor.js
+++ protractor.js
@@ -1,5 +1,31 @@
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+module.exports.config = {
+  allScriptsTimeout: 11000,
   specs: ['test/protractor/test.js']
+multiCapabilities: [{
+    'browserName': 'chrome'
+  }],
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  },
+  mocks: {
+    dir: 'mocks',
+    default: []
+  },
+  seleniumAddress: 'http://chromestandalone:4444/wd/hub',
+  plugins: [{
+      path: 'node_modules/protractor/plugins/accessibility',
+      chromeA11YDevTools: {
+        treatWarningsAsFailures: false
+      }
+    },
+    {
+      path: 'node_modules/protractor/plugins/ngHint',
+      asTests: true
+    },
+    {
+      path: 'node_modules/protractor/plugins/console',
+      failOnWarning: false,
+      failOnError: false
+    }]
 };
-
---
+++ protractor/runtests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /srv/www
+node_modules/.bin/protractor protractor.conf.js
+
